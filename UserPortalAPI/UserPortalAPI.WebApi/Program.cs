deee83729b76ce28036a2787a0a1b1f61920b698
Ertugrul Tosun <mail@ertugrultosun.com>
2022-04-02 22:42:50 +0300

    added services to program.cs

--- UserPortalAPI/UserPortalAPI.WebApi/Program.cs
+++ UserPortalAPI/UserPortalAPI.WebApi/Program.cs
@@ -1,3 +1,8 @@
+using UserPortalAPI.Application.Interfaces;
+using UserPortalAPI.Application.Services;
+using UserPortalAPI.Infrastructure.Interfaces;
+using UserPortalAPI.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
